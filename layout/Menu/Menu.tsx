320b265d32eca3c469625b678682b28aac737f13
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2021-08-14 20:07:02 +0600

    done

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,43 +1,46 @@
 import React from "react";
 import cn from 'classnames';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { AppContext } from "../../context/app.context";
 import { FirstLevelMenuItem, PageItem } from "../../interfaces/menu.interface";
-import { TopLevelCategory } from "../../interfaces/page.interface";
-
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-
+import { firstLevelMenu } from '../../helpers/helpers';
 import styles from './Menu.module.css';
 
 
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-  { route: 'products', name: 'Товары', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+
 
 export const Menu = (): JSX.Element => {
-  const { menu, firstCategory } = React.useContext(AppContext);
+  const { menu, setMenu, firstCategory } = React.useContext(AppContext);
+  const router = useRouter();
+
+  const openSecondLevel = (secondCategory: string) => {
+    setMenu && setMenu(menu.map(m => {
+      if (m._id.secondCategory === secondCategory) {
+        m.isOpened = !m.isOpened;
+      }
+      return m;
+    }));
+  };
 
   const buildFirstLevel = () => {
     return (
       <React.Fragment>
         {firstLevelMenu.map(menu => (
-          <div key = {menu.route}>
-            <a href = {`/${menu.route}`}>
-              <div className = {cn(styles.firstLevel, {
-                [styles.firstLevelActive]: menu.id === firstCategory
-              })}> 
-                { menu.icon } 
-                <span> { menu.name } </span>
-              </div>
-            </a>
-            { menu.id === firstCategory && buildSecondLevel(menu) }
-          </div>
-        ))}
+            <div key = {menu.route}>
+              <Link href = {`/${menu.route}`}>
+                <a>
+                  <div className = {cn(styles.firstLevel, {
+                    [styles.firstLevelActive]: menu.id === firstCategory
+                  })}> 
+                    { menu.icon } 
+                    <span> { menu.name } </span>
+                  </div>
+                </a>
+              </Link>
+              { menu.id === firstCategory && buildSecondLevel(menu) }
+            </div>
+          ))}
       </React.Fragment>
     );
   };
@@ -45,28 +48,37 @@ export const Menu = (): JSX.Element => {
   const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
     return (
       <div className = {styles.secondBlock}>
-        {menu.map(m => (
-          <div key = {m._id.secondCategory}>
-            <div className = {styles.secondLevel}> {m._id.secondCategory} </div>
-            <div className = {cn(styles.secondLevelBlock, {
-              [styles.secondLevelBlockOpened]: m.isOpened
-            })}>
-              { buildThirdLevel(m.pages, menuItem.route) }
+        {menu.map(m => {
+          if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+            m.isOpened = true;
+          }
+          return (
+            <div key = {m._id.secondCategory}>
+              <div className = {styles.secondLevel} onClick = {() => openSecondLevel(m._id.secondCategory)}> 
+                {m._id.secondCategory} 
+              </div>
+              <div className = {cn(styles.secondLevelBlock, {
+                [styles.secondLevelBlockOpened]: m.isOpened
+              })}>
+                { buildThirdLevel(m.pages, menuItem.route) }
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     );
   };
 
   const buildThirdLevel = (pages: PageItem[], route: string) => {
     return (
-      pages.map(p => (
-        <a href = {`/${route}/${p.alias}`} className = {cn(styles.thirdLevel, {
-          [styles.thirdLevelActive]: false 
-        })}>
-          {p.category}
-        </a>
+      pages.map((p, index) => (
+        <Link href = {`/${route}/${p.alias}`} key = {index}>
+          <a className = {cn(styles.thirdLevel, {
+            [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath 
+          })}>
+            {p.category}
+          </a>
+        </Link>
       ))
     );
   };
--- pages/courses/[alias].tsx
+++ pages/courses/[alias].tsx
@@ -1,31 +1,36 @@
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { ParsedUrlQuery } from "querystring";
+import { firstLevelMenu } from "../../helpers/helpers";
 import { MenuItem } from "../../interfaces/menu.interface";
-import { TopPageModel } from "../../interfaces/page.interface";
+import { TopLevelCategory, TopPageModel } from "../../interfaces/page.interface";
 import { ProductModel } from "../../interfaces/product.interface";
 import { withLayout } from "../../layout/Layout";
 
 
-const firstCategory = 0;
 
 const Course = ({ products }: CourseProps): JSX.Element => {
   return (
     <div>
-      { products.length }
+      { products && products.length }
     </div>
   );
 };
-
-
 
 export default withLayout(Course);
 
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const { data: menu } = await axios.post<MenuItem[]>('https://courses-top.ru/api/top-page/find', { firstCategory });
+  let paths: string[] = [];
+  for (const m of firstLevelMenu) {
+    const { data: menu } = await axios.post<MenuItem[]>('https://courses-top.ru/api/top-page/find', { 
+      firstCategory: m.id 
+    });
+    paths = paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
+  }
+  console.log(paths);
   return {
-    paths: menu.flatMap(m => m.pages.map(p => '/courses/' + p.alias)),
+    paths,
     fallback: true
   };
 };
@@ -36,25 +41,43 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({ params }: Ge
       notFound: true
     };
   }
-  const { data: menu } = await axios.post<MenuItem[]>('https://courses-top.ru/api/top-page/find', { firstCategory });
-  const { data: page } = await axios.get<TopPageModel>('https://courses-top.ru/api/top-page/byAlias/' + params.alias);
-  const { data: products } = await axios.post<ProductModel[]>('https://courses-top.ru/api/product/find', { category: page.category, limit: 10 });
 
-  return {
-    props: {
-      menu,
-      firstCategory,
-      page,
-      products,
+  const firstCategoryItem = firstLevelMenu.find(m => m.route === params.type);
+
+  if (!firstCategoryItem) {
+    return {
+      notFound: true
+    };
+  }
+  try {
+    const { data: menu } = await axios.post<MenuItem[]>('https://courses-top.ru/api/top-page/find', { firstCategory: firstCategoryItem.id });
+    if (menu.length === 0) {
+      return {
+        notFound: true
+      };
     }
-  };
+    const { data: page } = await axios.get<TopPageModel>('https://courses-top.ru/api/top-page/byAlias/' + params.alias);
+    const { data: products } = await axios.post<ProductModel[]>('https://courses-top.ru/api/product/find', { category: page.category, limit: 10 });
+    return {
+      props: {
+        menu,
+        firstCategory: firstCategoryItem.id,
+        page,
+        products,
+      }
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
 
 };
 
 
 interface CourseProps extends Record<string, unknown> {
   menu: MenuItem[];
-  firstCategory: number;
+  firstCategory: TopLevelCategory;
   page: TopPageModel;
   products: ProductModel[];
 }
